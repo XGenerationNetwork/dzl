b2e300e636aa9903c1bae550b20e420f7206a6dc
Mike Dzl <webmaster@xgeneration.net>
2015-09-25 08:02:28 -0400

    variable name bug fix

--- theme-settings.php
+++ theme-settings.php
@@ -3,6 +3,6 @@
 /**
  * Implements hook_form_system_theme_settings_alter().
  */
-function tweme_form_system_theme_settings_alter(&$form, &$form_state) {
+function dzl_form_system_theme_settings_alter(&$form, &$form_state) {
   $form['advanced']['bootstrap_cdn']['bootstrap_cdn']['#options']['3.3.4'] = '3.3.4';
 }
