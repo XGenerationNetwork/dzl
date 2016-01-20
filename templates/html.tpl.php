de50b0725ad38156a3254bbdd2802b9d7acd820a
Mike Dzl <webmaster@xgeneration.net>
2016-01-20 17:07:34 -0500

    New Year - New Fixes for Additional Support 1.) Added navigation bar CSS / LESS adjustments - placing navbar "fixed top" now pushes down logo & top region properly without overlap 2.) Added override.css file for bootstrap overrides 3.) Added IE9 graceful downgrade for Animate.css 4.) Disabled animate.css for smaller resolutions

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -7,7 +7,16 @@
     <title><?php print $head_title ?></title>
     <?php print $styles ?>
     <?php print $scripts ?>
+	<!--[if IE 9]>
+	<link type="text/css" rel="stylesheet" href="/sites/all/themes/dzl/ie9-noanimations.css">
+	<![endif]-->
 	<script src="http://ajax.aspnetcdn.com/ajax/modernizr/modernizr-2.7.2.js"></script>
+	<script>
+		Modernizr.load({
+		  test: Modernizr.cssanimations,
+		  nope: '/sites/all/themes/keuka/ie9-noanimations.css'
+		});
+	</script>
   </head>
   <body class="<?php print $classes ?>"<?php print $attributes ?>>
     <?php print $page_top ?>
